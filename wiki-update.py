64f1217a146152eac445b22a98277050246fa650
Joseph Reagle <joseph.2011@reagle.org>
2013-07-01 10:57:50 -0400

    element numbering

--- wiki-update.py
+++ wiki-update.py
@@ -140,7 +140,8 @@ def update_markdown(filename, md_fn):
             md_args.extend(['--bibliography'])
     elif 'cc/' in md_fn:
         md_args.extend(['--quash'])
-        md_args.extend(['--style-csl', 'chicago-note-bibliography'])
+        md_args.extend(['--number-elements'])
+        md_args.extend(['--style-csl', 'chicago-fullnote-bibliography'])
     elif 'syllabus' in md_fn:
         info("processing syllabus")
         mm_fn_html = md_fn.replace('syllabus', 'readings'
