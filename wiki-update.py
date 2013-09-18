566b7287f4edb3c0a60cd23e56607aa0a9fd5892
Joseph Reagle <joseph.2011@reagle.org>
2013-09-18 11:02:55 -0400

    tweaks to punctuation movement

--- wiki-update.py
+++ wiki-update.py
@@ -141,7 +141,7 @@ def update_markdown(filename, md_fn):
     elif 'cc/' in md_fn:
         # md_args.extend(['--quash'])
         md_args.extend(['--number-elements'])
-        #md_args.extend(['--style-csl', 'chicago-fullnote-only.csl'])
+        md_args.extend(['--punctuation-inside'])
         md_args.extend(['--style-csl', 'turabian-reagle.csl'])
     elif 'syllabus' in md_fn:
         info("processing syllabus")
