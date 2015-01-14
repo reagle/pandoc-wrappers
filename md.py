1e5d07dcb946ab0790defeb4ddd0a0163af2c229
Joseph Reagle <596814+reagle@users.noreply.github.com>
2015-01-14 11:30:16 -0500

    add potential params for reveal.js 3.0.0 transition

--- md.py
+++ md.py
@@ -271,12 +271,14 @@ def process(args):
         if args.presentation:
             args.validate = False
             args.css = False
-            # theme also in ~/joseph/talks/_custom/revealjs.css
             pandoc_opts.extend(['-t', 'revealjs', '--slide-level=2',
+                                # '--template=default.reveal3js',
+                                # '-V', 'revealjs-url=../_reveal3.js',
                                 '-V', 'revealjs-url=../_reveal.js',
                                 '-V', 'theme=beige',
                                 '-V', 'transition=linear',
-                                '-c', '../_custom/revealjs.css'])
+                                '-c', '../_custom/revealjs.css'
+                                ])
         if args.css:
             pandoc_opts.extend(['-c', args.css])
         if args.toc:
