3841a896bf01c4ea414c482f338dc700cc00552e
Joseph Reagle <596814+reagle@users.noreply.github.com>
2015-04-15 08:01:11 -0400

    move custom slide css to version 3

--- md.py
+++ md.py
@@ -271,7 +271,7 @@ def process(args):
         if args.presentation:
             args.validate = False
             args.css = False
-            pandoc_opts.extend(['-c', '../_custom/revealjs.css',
+            pandoc_opts.extend(['-c', '../_custom/reveal3js.css',
                                 '-t', 'revealjs', '--slide-level=2',
                                 '--template=default.reveal3js',
                                 '-V', 'revealjs-url=../_reveal3.js',
