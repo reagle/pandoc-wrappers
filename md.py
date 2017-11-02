cad7fdf8bec6b64bf188a776fe6383146aa2db6e
Joseph Reagle <596814+reagle@users.noreply.github.com>
2017-11-02 16:31:29 -0400

    handouts output format is now specified

--- md.py
+++ md.py
@@ -223,7 +223,7 @@ def create_talk_handout(abs_fn, tmp2_fn):
             else:
                 handout_f.write(line)
         handout_f.close()
-        md_cmd = ['md', '--divs', '-c',
+        md_cmd = ['md', '--divs', '-w', 'html', '-c',
                   'http://reagle.org/joseph/talks/_custom/class-handouts-201306.css',
                   handout_fn]
         info("md_cmd = %s" % ' '.join(md_cmd))
