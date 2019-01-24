9b97646ceb232373f85d5adadd6780465aaa3c58
Joseph Reagle <joseph.2011@reagle.org>
2019-01-24 10:01:52 -0500

    moving zim pack to python2 + zim-0.69 until gtk+3 copy/paste bug is addressed

--- wiki-update.py
+++ wiki-update.py
@@ -29,7 +29,7 @@ BROWSER = environ['BROWSER'] if 'BROWSER' in environ else None
 PANDOC_BIN = shutil.which("pandoc")
 MD_BIN = HOME + '/bin/pandoc-wrappers/md.py'
 # ZIM_BIN = '/usr/local/bin/zim'
-ZIM_BIN = '/usr/local/bin/python3 ' + HOME + '/bin/zim-desktop-wiki-master/zim.py'
+ZIM_BIN = '/usr/local/bin/python2 ' + HOME + '/bin/zim-0.69/zim.py'
 if not all([HOME, BROWSER, PANDOC_BIN, MD_BIN, ZIM_BIN]):
     raise FileNotFoundError("Your environment is not configured correctly")
 
