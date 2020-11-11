351908205c1b82697eb11f539a7cdd98b52136c3
reagle <joseph.acct@reagle.org>
2020-11-11 10:28:02 -0500

    removing 3.8 specificity

--- wiki-update.py
+++ wiki-update.py
@@ -40,9 +40,7 @@ BROWSER = environ["BROWSER"] if "BROWSER" in environ else None
 PANDOC_BIN = shutil.which("pandoc")
 MD_BIN = HOME + "/bin/pw/markdown-wrapper.py"
 # ZIM_BIN = '/usr/local/bin/zim'
-ZIM_BIN = (
-    "/usr/local/opt/python@3.8/bin/python3 " + HOME + "/bin/zim-latest/zim.py"
-)
+ZIM_BIN = "/usr/local/bin/python3 " + HOME + "/bin/zim-latest/zim.py"
 
 if not all([HOME, BROWSER, PANDOC_BIN, MD_BIN, ZIM_BIN]):
     raise FileNotFoundError("Your environment is not configured correctly")
