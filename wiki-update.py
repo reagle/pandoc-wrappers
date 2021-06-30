3c66d0ef8150a7a9862bd46a74dd9e1778140adc
reagle <596814+reagle@users.noreply.github.com>
2021-06-30 13:29:28 -0400

    new zim bin path using Rosetta source build in brew

--- wiki-update.py
+++ wiki-update.py
@@ -39,8 +39,7 @@ HOME = expanduser("~") if exists(expanduser("~")) else None
 BROWSER = environ["BROWSER"] if "BROWSER" in environ else None
 PANDOC_BIN = shutil.which("pandoc")
 MD_BIN = HOME + "/bin/pw/markdown-wrapper.py"
-# ZIM_BIN = "/usr/local/bin/zim"
-ZIM_BIN = "/usr/local/bin/python3 " + HOME + "/bin/zim-desktop-wiki/zim.py"
+ZIM_BIN = "/usr/local/Cellar/zim/0.73.5/bin/zim"
 
 if not all([HOME, BROWSER, PANDOC_BIN, MD_BIN, ZIM_BIN]):
     raise FileNotFoundError("Your environment is not configured correctly")
