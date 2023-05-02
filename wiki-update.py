08a4c707d24d58ab8a23c34d3ed8d10598a668f3
Joseph Reagle <596814+reagle@users.noreply.github.com>
2023-05-02 16:43:30 -0400

    remove other zim stuff, like retiring todos
    remove commented concurrent import

--- wiki-update.py
+++ wiki-update.py
@@ -9,19 +9,12 @@ zim/* (zim-wiki) -> html
 *.md (pandoc)-> html
 """
 
-import codecs
-import fnmatch
-import hashlib
 import logging
 import os
 import re
 import shutil
-import time
-
-# from concurrent import futures
 from os import chmod, environ, walk
 from os.path import (
-    abspath,
     exists,
     expanduser,
     getmtime,
@@ -29,21 +22,16 @@ from os.path import (
     splitext,
 )
 from pathlib import Path
-from subprocess import PIPE, Popen, call, check_output
-
-from lxml import etree, html  # type: ignore
+from subprocess import PIPE, Popen, call
 
 HOME = expanduser("~")
 BROWSER = environ["BROWSER"]
 PANDOC_BIN = shutil.which("pandoc")
 MD_BIN = HOME + "/bin/pw/markdown-wrapper.py"
-ZIM_BIN = "/Applications/Zim.app/Contents/MacOS/Zim"
 OBS_EXPORT_BIN = HOME + "/bin/obsidian-export"
 TEMPLATES_FOLDER = HOME + "/.pandoc/templates"
 
-if not all(
-    [HOME, BROWSER, PANDOC_BIN, MD_BIN, ZIM_BIN, OBS_EXPORT_BIN, TEMPLATES_FOLDER]
-):
+if not all([HOME, BROWSER, PANDOC_BIN, MD_BIN, OBS_EXPORT_BIN, TEMPLATES_FOLDER]):
     raise FileNotFoundError("Your environment is not configured correctly")
 
 log_level = logging.ERROR  # 40
@@ -56,7 +44,8 @@ info = logging.info
 debug = logging.debug
 
 #################################
-# Obsidianand Freeplane export functions
+# Obsidian export to markdown and run `markdown-wrapper.py`
+# on recent files
 #################################
 
 
@@ -73,7 +62,7 @@ def export_obsidian(vault_dir: str, export_dir: str) -> None:
     copy_mtime(vault_dir, export_dir)
 
 
-def update_markdown(files_to_process: list[str]) -> None:
+def invoke_md_wrapper(files_to_process: list[str]) -> None:
     """
     Configure arguments for `markdown-wrapper.py and invoke to convert
     markdown file to HTML
@@ -126,194 +115,52 @@ def update_markdown(files_to_process: list[str]) -> None:
             Path(tmp_body_fn).unlink()
 
 
-def check_markdown_files(source_dir: str) -> None:
-    """Convert any markdown file whose HTML file is older than it."""
-    # TODO: convert this to generic output, to work with html, docx, or odt!!
+def find_convert_md(source_dir: str) -> None:
+    """Find and convert any markdown file whose HTML file is older than it."""
+    # TODO: have this work when output format is docx or odt.
     # 2020-03-11: attempted but difficult, need to:
     #     - ignore when a docx was converted to html (impossible?)
     #     - don't create outputs if they don't already exist
     #     - don't update where docx is newer than md, but older than html?
     # 2020-09-17: possible hack: always generate HTML in addition to docx
 
-    files_bare = [splitext(fn_md)[0] for fn_md in locate("*.md", source_dir)]
+    source_path = Path(source_dir)
     files_to_process = []
-    for fn_bare in files_bare:
-        fn_md = fn_bare + ".md"
-        fn_html = fn_bare + ".html"
-        if exists(fn_html):
-            if getmtime(fn_md) > getmtime(fn_html):
+
+    for fn_md in source_path.glob("**/*.md"):
+        fn_html = fn_md.with_suffix(".html")
+        if fn_html.exists():
+            if fn_md.stat().st_mtime > fn_html.stat().st_mtime:
                 debug(
-                    f"""{fn_md} {getmtime(fn_md)} """
-                    + f"""> {fn_html} {getmtime(fn_html)}"""
+                    f"""{fn_md} {fn_md.stat().st_mtime} """
+                    + f"""> {fn_html} {fn_html.stat().st_mtime}"""
                 )
                 files_to_process.append(fn_md)
 
     info(f"{files_to_process=}")
-    update_markdown(files_to_process)
+    invoke_md_wrapper(files_to_process)
+
+
+def create_missing_html_files(folder: str) -> None:
+    """
+    Walk a folder looking for markdown files. For each markdown file without a
+    corresponding HTML file, create one, and set its mtime back so
+    `find_convert_md_files` knows to process it.
+
+    """
+    folder_path = Path(folder)
+
+    for md_file in folder_path.glob("**/*.md"):
+        html_file = md_file.with_suffix(".html")
+
+        if not html_file.exists():
+            html_file.touch()
+            os.utime(html_file, (0, 0))
 
 
 #################################
-# Updating tasks to my plan home page
+# Utilities
 #################################
-
-
-def log2work(done_tasks):
-    """
-    Log completed zim tasks to work microblog
-    """
-
-    RE_MARKUP = re.compile(r"""(?P<link>\[\[(?P<url>.*?)\|(?P<text>.*?)\]\])""")
-
-    log_items = []
-    for activity, task in done_tasks:
-        # convert zim wiki markup to HTML (hack)
-        task = re.sub(r"(?<!:)\/\/(.*?)\/\/", r"<em>\1</em>", task)  # italics
-        task = re.sub(r"\*\*(.*?)\*\*", r"<strong>\1</strong>", task)  # bold
-        task = RE_MARKUP.sub(r'<a href="\g<url>">\g<text></a>', task)  # link
-
-        date_token = get_Now_YMD()
-        digest = hashlib.md5(task.encode("utf-8", "replace")).hexdigest()
-        uid = "e" + date_token + "-" + digest[:4]
-        log_item = (
-            f"""<li class="event" id="{uid}">"""
-            f"""{date_token}: {activity}] {task}</li>\n"""
-        )
-        info(f"{log_item=}")
-        log_items.append(log_item)
-
-    OUT_FILE = HOME + "/data/2web/reagle.org/joseph/plan/plans/index.html"
-    plan_fd = codecs.open(OUT_FILE, "r", "utf-8", "replace")
-    plan_content = plan_fd.read()
-    plan_fd.close()
-
-    insertion_regexp = re.compile(r"(<h2>Done Work</h2>\s*<ul>)")
-
-    new_content = insertion_regexp.sub(
-        "\\1 \n  %s" % "".join(log_items),
-        plan_content,
-        re.DOTALL | re.IGNORECASE,
-    )
-    if new_content:
-        fd = codecs.open(OUT_FILE, "w", "utf-8", "replace")
-        fd.write(new_content)
-        fd.close()
-    else:
-        print("Sorry, XML insertion failed.")
-
-
-def retire_tasks(directory):
-    """
-    Removes completed '[x]' tasks from zim
-    """
-    if "zim" in check_output(["ps", "axw"]).decode("utf-8").lower():
-        print(
-            f"exporting {directory}, Zim is running,"
-            + " skipping task retirement and export"
-        )
-        return False
-    else:
-        zim_files = locate("*.txt", directory)
-        for zim_fn in zim_files:
-            debug(zim_fn)
-            done_tasks = []
-            activity = "misc"
-            new_wiki_page = []
-            with open(zim_fn) as wiki_page:
-                for line in wiki_page:
-                    label = re.search(r"@\w+", line)
-                    # TODO: support multiple labels and remove from activity
-                    if label:
-                        activity = "#" + label.group(0).strip()[1:]
-                    # look for completed or cancelled events
-                    if "[x]" in line:  # other signs "[*]", "[<]", "[>]"
-                        item = line.split("]", 1)[1].strip()
-                        debug("found item %s" % item)
-                        debug("activity = %s" % activity)
-                        done_tasks.append((activity, item))
-                    else:
-                        new_wiki_page.append(line)
-            if done_tasks:
-                new_wiki_page_fd = open(zim_fn, "w")
-                new_wiki_page_fd.writelines("%s" % line for line in new_wiki_page)
-                new_wiki_page_fd.close()
-                log2work(done_tasks)
-        return True
-
-
-def grab_todos(filename):
-    """Grab ongoing todos from zim output for insertion in plan web page."""
-    debug("grab_todos")
-    html_parser = etree.HTMLParser(remove_comments=True, remove_blank_text=True)
-    doc = etree.parse(open(filename, "rb"), html_parser)
-    div = doc.xpath('//div[@id="zim-content-body"]')[0]
-    div.set("id", "Ongoing-todos")
-    div_txt = etree.tostring(div).decode("utf-8")
-    div_txt = div_txt.replace('href="./', 'href="../zwiki/')
-    div_txt = div_txt.replace('href="file:///Users/reagle/joseph/', 'href="../../')
-    new_div = html.fragment_fromstring(div_txt)
-    return new_div
-
-
-def insert_todos(plan_fn, todos):
-    debug("insert_todos")
-    html_parser = etree.HTMLParser(remove_comments=True, remove_blank_text=True)
-    doc = etree.parse(open(plan_fn, "rb"), html_parser)
-    div = doc.xpath('//div[@id="Ongoing-todos"]')[0]
-    parent = div.getparent()
-    parent.replace(div, todos)
-    doc.write(plan_fn)
-
-
-#################################
-# Utility functions
-#################################
-
-
-def get_Now_YMD():
-    """Now in YearMonthDay format"""
-
-    now = time.localtime()
-    date_token = time.strftime("%y%m%d", now)
-    return date_token
-
-
-def locate(pattern, root):
-    """Locate all files matching supplied filename pattern in and below
-    supplied root directory."""
-    # TODO: move RE instead of fnmatch?
-    for path, _, files in walk(abspath(root)):
-        # debug(f"{path=}")
-        for fn in fnmatch.filter(files, pattern):
-            yield join(path, fn)
-
-
-def has_dir_changed(directory: str) -> bool:
-    """Check if a directory has changed by m5dsumming the directory
-    listing and leaving a copy to compare with next run.
-    (Used for checking changes of Zim wiki.)
-    """
-
-    debug("dir   = %s" % directory)
-    checksum_file = directory + ".dirs.md5sum"
-    checksum = Popen(
-        ["ls -l -R %s | md5sum" % directory], shell=True, stdout=PIPE
-    ).communicate()[0]
-    checksum = checksum.split()[0].decode("utf-8")
-    if not exists(checksum_file):
-        open(checksum_file, "w").write(checksum)
-        debug("checksum created %s" % checksum)
-        return True
-    else:
-        debug("checksum_file = '%s'" % checksum_file)
-        state = open(checksum_file).read()
-        debug("state = %s" % state)
-        if checksum == state:
-            debug("checksum == state")
-            return False
-        else:
-            open(checksum_file, "w").write(checksum)
-            debug("checksum updated")
-            return True
 
 
 def chmod_recursive(path, dir_perms, file_perms):
@@ -328,7 +175,7 @@ def chmod_recursive(path, dir_perms, file_perms):
 
 def copy_mtime(source_dir: str, target_dir: str) -> None:
     """
-    Copy mtime from source_dir to target_dir so that `check_markdown_files`
+    Copy mtime from source_dir to target_dir so that `find_convert_md_files`
     know what changed.
     """
     src_path = Path(source_dir)
@@ -344,27 +191,11 @@ def copy_mtime(source_dir: str, target_dir: str) -> None:
             dst_fn = dst_path.joinpath(relative_path)
 
             if dst_fn.exists() and dst_fn.is_file():
-                # Get the modified time of the source file
+                # find_convert_md_f time of the source file
                 src_mtime = src_fn.stat().st_mtime
 
                 # Apply the modified time to the destination file
                 os.utime(dst_fn, (dst_fn.stat().st_atime, src_mtime))
-
-
-def create_missing_html_files(folder: str) -> None:
-    """
-    Walk a folder looking for markdown files. For each markdown file without a
-    corresponding HTML file, create one, and set its mtime back so
-    `check_markdown_files` knows to process it.
-    """
-    folder_path = Path(folder)
-
-    for md_file in folder_path.glob("**/*.md"):
-        html_file = md_file.with_suffix(".html")
-
-        if not html_file.exists():
-            html_file.touch()
-            os.utime(html_file, (0, 0))
 
 
 ##################################
@@ -451,8 +282,8 @@ if __name__ == "__main__":
 
     # Public markdown files
     source_dir = HOME + "/joseph/"
-    check_markdown_files(source_dir)
+    find_convert_md(source_dir)
 
     # Private markdown files
     HOMEDIR = HOME + "/data/1work/"
-    check_markdown_files(HOMEDIR)
+    find_convert_md(HOMEDIR)
