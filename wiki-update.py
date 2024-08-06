afd41d0d59c75fa7ff6b74027c8aa6e5e9ec4a4a
Joseph Reagle <596814+reagle@users.noreply.github.com>
2024-08-06 15:59:06 -0400

    refactor: tweaks

--- wiki-update.py
+++ wiki-update.py
@@ -7,12 +7,10 @@ ob-/* (obsidian) -> html
 *.md (pandoc)-> html
 """
 
-import logging
+import logging as log
 import os
 import re
 import shutil
-from os import chmod
-from os.path import join
 from pathlib import Path
 from subprocess import PIPE, Popen, call
 
@@ -29,15 +27,6 @@ TEMPLATES_FOLDER = HOME / ".pandoc/templates"
 if not all([HOME, BROWSER, PANDOC_BIN, MD_BIN, OBS_EXPORT_BIN, TEMPLATES_FOLDER]):
     raise FileNotFoundError("Your environment is not configured correctly")
 
-log_level = logging.ERROR  # 40
-
-# function aliases
-critical = logging.critical
-error = logging.error
-warning = logging.warning
-info = logging.info
-debug = logging.debug
-
 #################################
 # Export Obsidian markdown to standard markdown.
 #################################
@@ -46,7 +35,7 @@ debug = logging.debug
 def export_obsidian(vault_dir: Path, export_dir: Path) -> None:
     """Call obsidian-export on source; copy source's mtimes to target."""
     export_cmd = f"{OBS_EXPORT_BIN} {vault_dir} {export_dir}"
-    info(f"{export_cmd=}")
+    log.info(f"{export_cmd=}")
 
     print(f"exporting {vault_dir}")
     results = Popen((export_cmd), stdout=PIPE, stderr=PIPE, shell=True, text=True)
@@ -58,19 +47,17 @@ def export_obsidian(vault_dir: Path, export_dir: Path) -> None:
     remove_empty_or_hidden_folders(export_dir)
     review_created_or_deleted_files(vault_dir, export_dir)
     if has_dir_changed(export_dir):
-        warning(f"{dir=} has changed")
+        log.warning(f"{dir=} has changed")
         create_index(vault_dir, export_dir)
 
 
 def create_index(vault_path: Path, export_path: Path) -> None:
     """Create a new HTML index for the export vault."""
-    # TODO: this seems to have problems when filenames have spaces
-    #   2023-05-06: identified
-
-    info(f"creating index for {vault_path}")
+    log.info(f"creating index for {vault_path}")
     vault_index_file = vault_path / "_index.md"
     export_index_file = export_path / "_index.md"
-    with open(vault_index_file, "w") as output_file:
+
+    with vault_index_file.open("w") as output_file:
         output_file.write(f"# Index of {vault_path.name}\n")
         for path in vault_path.glob("**/*.md"):
             relative_path = path.relative_to(vault_path)
@@ -78,11 +65,12 @@ def create_index(vault_path: Path, export_path: Path) -> None:
             depth = len(relative_path.parts) - 1
             indentation = "  " * depth
             output_file.write(f"{indentation}- {link_text}\n")
+
     shutil.copy2(vault_index_file, export_index_file)
-    info(f"created {output_file=} and {export_index_file=}")
-    debug(
-        f"""{vault_index_file} {vault_index_file.stat().st_mtime} """
-        + f"""> {export_index_file} {export_index_file.stat().st_mtime}"""
+    log.info(f"created {output_file=} and {export_index_file=}")
+    log.debug(
+        f"{vault_index_file} {vault_index_file.stat().st_mtime} "
+        f"> {export_index_file} {export_index_file.stat().st_mtime}"
     )
 
 
@@ -106,22 +94,20 @@ def find_convert_md(source_path: Path) -> None:
         fn_html = fn_md.with_suffix(".html")
         if fn_html.exists():
             if fn_md.stat().st_mtime > fn_html.stat().st_mtime:
-                debug(
+                log.debug(
                     f"""{fn_md} {fn_md.stat().st_mtime} """
                     + f"""> {fn_html} {fn_html.stat().st_mtime}"""
                 )
                 files_to_process.append(fn_md)
 
-    info(f"{files_to_process=}")
+    log.info(f"{files_to_process=}")
     invoke_md_wrapper(files_to_process)
 
 
 def invoke_md_wrapper(files_to_process: list[Path]) -> None:
-    """Configure arguments for `markdown-wrapper.py` and invoke to convert
-    markdown file to HTML.
-    """
+    """Configure arguments for `markdown-wrapper.py` and invoke."""
     for fn_md in files_to_process:
-        info(f"updating fn_md {fn_md}")
+        log.info(f"updating fn_md {fn_md}")
         path_md = Path(fn_md)
         content = path_md.read_text()
         md_cmd = [MD_BIN]
@@ -157,7 +143,7 @@ def invoke_md_wrapper(files_to_process: list[Path]) -> None:
         match_md_opts = re.search('^md_opts_: "?(.*)"?', content, re.MULTILINE)
         if match_md_opts:
             md_opts = match_md_opts.group(1).strip().split(" ")
-            debug(f"{md_opts=}")
+            log.debug(f"{md_opts=}")
             md_args.extend(md_opts)
         md_cmd.extend(md_args)
         md_cmd.extend([path_md])
@@ -173,7 +159,7 @@ def invoke_md_wrapper(files_to_process: list[Path]) -> None:
 
 
 def remove_chunks(soup, selectors: list[str]) -> None:
-    """Remove chunks of HTML give CSS selectors"""
+    """Remove chunks of HTML give CSS selectors."""
     for selector in selectors:
         chunks = soup.select(selector)
         for chunk in chunks:
@@ -217,7 +203,7 @@ def transclude(
 
 def has_dir_changed(path: Path) -> bool:
     """Check if content of folder has changed."""
-    info(f"{path=}")
+    log.info(f"{path=}")
     if not path.is_dir():
         raise NotADirectoryError(f"{path} is not a directory")
 
@@ -230,19 +216,19 @@ def has_dir_changed(path: Path) -> bool:
     if not checksum_file.exists():
         with checksum_file.open("w") as file:
             file.write(checksum)
-        debug(f"checksum created {checksum}")
+        log.debug(f"checksum created {checksum}")
         return True
     else:
-        debug(f"{checksum_file=}")
+        log.debug(f"{checksum_file=}")
         state = checksum_file.read_text()
-        debug(f"{state=}")
+        log.debug(f"{state=}")
         if checksum == state:
-            debug("checksum == state")
+            log.debug("checksum == state")
             return False
         else:
             with checksum_file.open("w") as file:
                 file.write(checksum)
-            debug("checksum updated")
+            log.debug("checksum updated")
             return True
 
 
@@ -255,64 +241,68 @@ def remove_empty_or_hidden_folders(path: Path, hide_prefix: str = "_") -> bool:
     def is_empty(folder: Path) -> bool:
         return not any(folder.iterdir())
 
-    info(f"check for empty or hidden folders {path=}")
+    log.info(f"check for empty or hidden folders {path=}")
     did_remove = False
     folders = sorted(path.rglob("**/"))  # returns all descendant folders
     for folder in folders:
         if is_empty(folder) or folder.name.startswith(hide_prefix):
             shutil.rmtree(folder)
             did_remove = True
-            info(f"  Removed folder: {folder}")
+            log.info(f"  Removed folder: {folder}")
     return did_remove
 
 
 def review_created_or_deleted_files(src_path: Path, dst_path: Path) -> bool:
-    """Check dst_path and create or delete HTML files based on the
-    presence of their corresponding markdown in src_path.
-    Created HTML is set with an early mtime so find_convert_md_files()
-    knows to process it.
+    """Review for created or deleted file.
+
+    Check dst_path and create or delete HTML files based on the presence of
+    their corresponding markdown in src_path.
+    Created HTML is set with an early mtime so find_convert_md_files() knows
+    to process it.
     (Renamed files are simply deleted and created.)
     """
     has_changed = False
-    info(f"checking for new markdown files in {dst_path}")
+    log.info(f"checking for new markdown files in {dst_path}")
     for dst_md_file in dst_path.glob("**/*.md"):
-        info(f"  {dst_md_file=}")
+        log.info(f"  {dst_md_file=}")
         html_file = dst_md_file.with_suffix(".html")
         if not html_file.exists():
             html_file.touch()
             os.utime(html_file, (0, 0))
-            info(f"created {html_file}")
+            log.info(f"created {html_file}")
             has_changed = True
 
-    info(f"checking for deleted markdown files in {src_path}")
+    log.info(f"checking for deleted markdown files in {src_path}")
     for dst_md_file in dst_path.glob("**/*.md"):
         src_md_file = src_path / dst_md_file.relative_to(dst_path)
         if not src_md_file.exists():
             dst_md_file.unlink()
             dst_md_file.with_suffix(".html").unlink()
-            info(f"deleted {dst_md_file}")
+            log.info(f"deleted {dst_md_file}")
             has_changed = True
 
     return has_changed
 
 
 def chmod_recursive(
-    path: Path, dir_perms: int = 0o755, file_perms: int = 0o744
+    path: Path, dir_perms: int = 0o755, file_perms: int = 0o644
 ) -> None:
     """Fix permissions on a generated/exported tree if needed."""
-    debug(f"changing perms to {dir_perms};{file_perms} on {path=}")
-    for root, dirs, files in os.walk(path):  # does os.walk accept Path?
-        for d in dirs:
-            chmod(join(root, d), dir_perms)
-        for f in files:
-            chmod(join(root, f), file_perms)
+    log.debug(f"changing perms to {dir_perms:o};{file_perms:o} on {path=}")
+
+    for item in path.rglob("*"):
+        if item.is_dir():
+            item.chmod(dir_perms)
+        elif item.is_file():
+            item.chmod(file_perms)
 
 
 def copy_mtime(src_path: Path, dst_path: Path) -> None:
-    """Copy mtime from source_dir to target_dir so that `find_convert_md_files`
-    know what changed.
+    """Copy mtime from source_dir to target_dir.
+
+    This lets `find_convert_md_files` know what changed.
     """
-    debug("copying mtimes")
+    log.debug("copying mtimes")
     if not src_path.is_dir() or not dst_path.is_dir():
         raise ValueError("Both arguments should be valid directory paths.")
 
@@ -330,16 +320,16 @@ def copy_mtime(src_path: Path, dst_path: Path) -> None:
                 os.utime(dst_fn, (dst_fn.stat().st_atime, src_mtime))
 
 
-def reset_folder(folder_path):
+def reset_folder(folder_path: Path) -> None:
     """Remove and remake a folder."""
-    info(f"removing/recreating {folder_path=}")
+    log.info(f"removing/recreating {folder_path=}")
     if folder_path.exists():
         shutil.rmtree(folder_path)
     folder_path.mkdir(parents=True, exist_ok=True)
     if folder_path.exists():
-        info(f"{folder_path} creation succeeded.")
+        log.info(f"{folder_path} creation succeeded.")
     else:
-        info(f"{folder_path} creation failed.")
+        log.info(f"{folder_path} creation failed.")
 
 
 ##################################
@@ -392,23 +382,18 @@ if __name__ == "__main__":
     arg_parser.add_argument("--version", action="version", version="1.0")
     args = arg_parser.parse_args()
 
-    log_level = logging.ERROR  # 40
-    if args.verbose == 1:
-        log_level = logging.WARNING  # 30
-    elif args.verbose == 2:
-        log_level = logging.INFO  # 20
-    elif args.verbose >= 3:
-        log_level = logging.DEBUG  # 10
-    LOG_FORMAT = "%(module).5s %(levelname).3s %(funcName).5s: %(message)s"
+    log_level = (log.CRITICAL) - (args.verbose * 10)
+    LOG_FORMAT = "%(levelname).4s %(funcName).10s:%(lineno)-4d| %(message)s"
     if args.log_to_file:
-        logging.basicConfig(
-            filename="wiki-update.log",
+        print("logging to file")
+        log.basicConfig(
+            filename="markdown-wrapper.log",
             filemode="w",
             level=log_level,
             format=LOG_FORMAT,
         )
     else:
-        logging.basicConfig(level=log_level, format=LOG_FORMAT)
+        log.basicConfig(level=log_level, format=LOG_FORMAT)
 
     ## Obsidian vault ##
 
@@ -440,5 +425,4 @@ if __name__ == "__main__":
         remove_selectors=["div#obsidian-footer", "header"],
     )
     if modified_html:
-        with open(planning_page, "w") as f:
-            f.write(modified_html)
+        planning_page.write_text(modified_html)
